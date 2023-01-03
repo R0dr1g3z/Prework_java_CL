b7798d84e12f21d8b13c8a95241237040de0a409
R0dr1g3z <r0dr1g3zek@gmail.com>
2023-01-03 22:58:05 +0100

    add Operators05.java

---
+++ src/Operators05.java
@@ -0,0 +1,8 @@
+public class Operators05 {
+    public static void main(String[] args) {
+        double nr1 = 5.1;
+        float nr2 = 5.0f;
+        boolean result = nr1 > nr2;
+        System.out.println(result);
+    }
+}
