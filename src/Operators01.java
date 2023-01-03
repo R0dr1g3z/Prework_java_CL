e42938ad0b689f42f4443cce5ce792359f084a7d
R0dr1g3z <r0dr1g3zek@gmail.com>
2023-01-03 22:43:09 +0100

    add Operators

---
+++ src/Operators01.java
@@ -0,0 +1,15 @@
+public class Operators01 {
+    public static void main(String[] args) {
+        int int1 = 25;
+        char char2 = 'c';
+        long long3 = 509840923;
+        double double4 = 12.2;
+        boolean boolean5 = true;
+
+        System.out.println(int1);
+        System.out.println(char2);
+        System.out.println(long3);
+        System.out.println(double4);
+        System.out.println(boolean5);
+    }
+}
---
+++ src/Operators02.java
@@ -0,0 +1,8 @@
+public class Operators02 {
+    public static void main(String[] args) {
+        byte nr1 = 94;
+        short nr2 = 257;
+        int result = nr1 + nr2;
+        System.out.println(result);
+    }
+}
