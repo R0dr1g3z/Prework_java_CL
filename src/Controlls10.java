77e1c2c720b0a673b46e8cf3502f9290c14fb5cf
R0dr1g3z <r0dr1g3zek@gmail.com>
2023-01-04 00:59:46 +0100

    add Controlls10.java

---
+++ src/Controlls10.java
@@ -0,0 +1,23 @@
+public class Controlls10 {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (j >= i + 1){
+                    System.out.print(j + " ");
+                } else 
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (j + i > n){
+                    System.out.print(j + " ");
+                } else
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
