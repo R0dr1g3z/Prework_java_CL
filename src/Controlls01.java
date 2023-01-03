e7d298ba359bcf3aefe9876bddabeb117815e7a7
R0dr1g3z <r0dr1g3zek@gmail.com>
2023-01-03 23:21:02 +0100

    add Controlls01.java

---
+++ src/Controlls01.java
@@ -0,0 +1,11 @@
+public class Controlls01 {
+    public static void main(String[] args) {
+        int nr1 = 3;
+        int nr2 = 7;
+        if (nr1 > nr2) {
+            System.out.println("Wieksza liczba to " + nr1);
+        } else {
+            System.out.println("Większa liczba to " + nr2);
+        }
+    }
+}
