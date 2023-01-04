abbe888b251c8a07db830aedc0fa93d7e9f9278a
R0dr1g3z <r0dr1g3zek@gmail.com>
2023-01-04 13:56:31 +0100

    add Tables01.java

---
+++ src/Tables01.java
@@ -0,0 +1,7 @@
+public class Tables01 {
+    public static void main(String[] args) {
+        int[] threeElements = {3, 4, 5};
+        String space = " ";
+        System.out.print(threeElements[0] + space + threeElements[1] + space + threeElements[2]);
+    }
+}
