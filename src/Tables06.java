0f86cc5a184eecc76291e573bf5ee43a8727ee9c
R0dr1g3z <r0dr1g3zek@gmail.com>
2023-01-04 14:30:07 +0100

    add Tables06.java

---
+++ src/Tables06.java
@@ -0,0 +1,8 @@
+public class Tables06 {
+    public static void main(String[] args) {
+        int[] numbers = {4, 643, 112, 9999, -69};
+        for (int i = numbers.length-1; i >= 0; i--) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
